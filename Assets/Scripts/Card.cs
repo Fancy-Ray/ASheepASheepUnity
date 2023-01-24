5e3b497fe4cd20efa32ffb7363a7e8d904210bb6
Fancy-Ray <1273117205@qq.com>
2023-01-24 17:22:45 +0800

    finnish

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,13 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Assets.Scripts;
 using UnityEngine;
+using Assets.Scripts.Main;
+using System;
+using System.Reflection;
 
 public class Card : MonoBehaviour
 {
+    public GameObject parent;
     private void OnMouseDown()
     {
-        Debug.Log("111");
+        if (Main.index == false)
+        {   
+            for (int index = 0; index < 7; index++)
+            {
+                bool coordinatesAreRepeated = false;
+                for (int index_ = 0; index_ < Main.zone1.cardNumber; index_++)
+                {
+                    try
+                    {
+                        if (Main.zone1.correspond[Main.zone1.cards[index_]].x == Main.zone1.vector3sForTrough[index].x && Main.zone1.correspond[Main.zone1.cards[index_]].y == Main.zone1.vector3sForTrough[index].y && Main.zone1.correspond[Main.zone1.cards[index_]].z == Main.zone1.vector3sForTrough[index].z)
+                            coordinatesAreRepeated = true;
+                        //if (Main.zone1.cards[index_] == null)
+                        //    coordinatesAreRepeated = false;
+                    }
+                    catch { }
+                }
+                if (coordinatesAreRepeated == false)
+                {
+                    Main.index = true;
+                    try
+                    {
+                        Main.zone1.correspond[parent] = Main.zone1.vector3sForTrough[index];
+                    }
+                    catch { }
+                    break;
+                }
+            }
+        }
 
     }
     private void OnMouseOver()
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,33 +4,62 @@ using UnityEngine;
 using Assets.Scripts;
 using System;
 using System.Linq.Expressions;
+using System.Reflection;
+using UnityEngine.XR;
 
-public class Main : MonoBehaviour
+namespace Assets.Scripts.Main
 {
-    public GameObject card;
-    static bool index { set; get; } = true;
-    public Sprite[] sprites;
-    private Vector3[] coordinates=new Vector3[6];//存贮最开始创建cards选择的坐标
-    public InstanceZonesRequired zone1 = new InstanceZonesRequired(9, new Vector3[4] { new Vector3(0, 4, 0), new Vector3(-4, 4, 0), new Vector3(-2, 4, 0), new Vector3(2,4,0)},new int[4] { 3,2,2,2});
-    GameObject card0;
-    // Start is called before the first frame update
-    void Start()
+    public class Main : MonoBehaviour
     {
-        try
+        public GameObject card;
+        public static bool index { set; get; } = true;
+        public Sprite[] sprites;
+        private Vector3[] coordinates = new Vector3[6];//存贮最开始创建cards选择的坐标
+        public static InstanceZonesRequired zone1;
+        GameObject card0;
+        // Start is called before the first frame update
+        void Start()
         {
-            zone1.CreateCards(sprites, card, GameObject.Find("trough").transform);
-        }
-        catch (Exception e) { 
-        Debug.LogException(e);
-            index= false;
-        }
-        
-    }
+            zone1 = new InstanceZonesRequired(54, sprites,
+                new Vector3[6] {
+                    //new Vector3(0, 2, 0), new Vector3(-4, 2, 0), new Vector3(-2, 2, 0), new Vector3(2, 2, 0),new Vector3(4, 2, 0),
+                    //new Vector3(0, 4, 0), new Vector3(-4, 4, 0), new Vector3(-2, 4, 0), new Vector3(2, 4, 0),new Vector3(4, 4, 0),
+                    //new Vector3(0, 6, 0), new Vector3(-4, 6, 0), new Vector3(-2, 6, 0), new Vector3(2, 6, 0),new Vector3(4, 6, 0),
+                   new Vector3(0, 4, 0) , new Vector3(-2, 4, 0), new Vector3(2, 4, 0),
+                   new Vector3(0, 6, 0) , new Vector3(-2, 6, 0), new Vector3(2, 6, 0),
+                    //new Vector3(0, 8, 0), new Vector3(-4, 8, 0), new Vector3(-2, 8, 0), new Vector3(2, 8, 0),new Vector3(4, 8, 0),
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(index)
-            index= zone1.DropCards();
+                }, new int[6] {
+                    //3, 2, 2, 3, 2,
+                    //4, 4, 6, 4, 4,
+                    //6, 5, 4, 4, 4,
+                    8, 12, 8,
+                    10, 8, 8,
+                    //2, 2, 2, 2, 3
+                })
+            {
+
+            };
+            try
+            {
+                zone1.CreateCards(card, GameObject.Find("trough").transform);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                index = false;
+            }
+
+
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (index)
+            {
+                index = zone1.DropCards();
+                zone1.DestroyCards();
+            }
+        }
     }
 }
